5b8b970b9ba71256f807415894bab2f5e54c2a98
PEM-- <pemarchandet@gmail.com>
2016-02-07 19:36:23 +0100

    Caching strategy

--- app/entry/client/root.jsx
+++ app/entry/client/root.jsx
@@ -2,6 +2,16 @@ import routes from './routes';
 import Wrapper from 'RootEnv/reduxWrapper';
 import storeBuilder from 'RootEnv/storeBuilder';
 
+// Accounts options
+Accounts.config({
+  sendVerificationEmail: true,
+  forbidClientAccountCreation: false
+});
+
+// Global subscription
+
+
+
 Meteor.startup(() => {
   const clientOptions = {
     wrapper: Wrapper,
