72a647f012356305c1f94d040f0a37f6eeec1a2b
PEM-- <pemarchandet@gmail.com>
2016-02-07 15:26:19 +0100

    Footer - pending

--- app/ClientApp/client/components/Footer.jsx
+++ app/ClientApp/client/components/Footer.jsx
@@ -4,18 +4,49 @@ import {palette, styles} from '../../../entry/client/styles/index';
 import FontAwesome from 'react-fontawesome';
 const Icon = radium(FontAwesome);
 
+import AnimatedLink from './AnimatedLink';
 import MaximizedContainer from './MaximizedContainer';
 
 const Footer = radium(() => (
   <div
     style={[
       styles.flexItemStaticSize,
-      styles.primary2Color
+      styles.primary2Color,
+      {
+        padding: '20px 0',
+        fontSize: '90%'
+      },
     ]}
   >
     <MaximizedContainer>
-      <h1 style={[styles.lisibility]}>Footer</h1>
-      <Icon name='rocket' size='2x' spin style={[styles.lisibility]} />
+      <div style={[
+        styles.flex, styles.flex.row, {flexWrap: 'wrap'}
+      ]}>
+        <div style={[
+          styles.flexItemDynamicSize, styles.flex, styles.flex.row,
+          {
+            justifyContent: 'space-between',
+            marginRight: '100px'
+          }
+        ]}>
+          <AnimatedLink to='/legal-mentions' isCream={true}>Mentions légales</AnimatedLink>
+          <AnimatedLink to='/cookies' isCream={true}>Confidentialité</AnimatedLink>
+          <AnimatedLink to='/cgv' isCream={true}>Conditions de ventes</AnimatedLink>
+        </div>
+        <div style={[
+          styles.flexItemStaticSize,
+          styles.flex, styles.flex.row,
+          {
+            justifyContent: 'space-between',
+            minWidth: '120px'
+          }
+        ]}>
+          <Icon name='twitter' size='lg' style={[styles.flexItemStaticSize, styles.lisibility]} />
+          <Icon name='facebook' size='lg' style={[styles.flexItemStaticSize, styles.lisibility]} />
+          <Icon name='linkedin' size='lg' style={[styles.flexItemStaticSize, styles.lisibility]} />
+          <Icon name='envelope' size='lg' style={[styles.flexItemStaticSize, styles.lisibility]} />
+        </div>
+      </div>
     </MaximizedContainer>
   </div>
 ));
