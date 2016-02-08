bf570530889cb0c5a2776d9104420bc634472a87
PEM-- <pemarchandet@gmail.com>
2016-02-08 14:41:11 +0100

    Main menu button

--- app/ClientApp/client/components/Header/NavButton.jsx
+++ app/ClientApp/client/components/Header/NavButton.jsx
@@ -12,142 +12,130 @@ function pathCommands(instructions) {
 }
 
 function computeValues(completion, props) {
-  let {size, thickness} = props;
+    let { size, thickness } = props;
 
-  let originX = size / 2;
-  let radius = size / 2 - thickness;
-  let circumference = 2 * radius * Math.PI;
-
-  return {
-    circleDashOffset: circumference * completion,
-    circleRotation: 180 + 180 * completion,
-    leftPathRotation: 45 - 45 * completion,
-    rightPathRotation: 45 - 45 * completion,
-    rightPathX: originX - radius * 0.7 * completion,
-    rightPathStartX: radius * 0.4 * completion,
-    rightPathStartY: radius * -0.7 + radius * 0.3 * completion,
-    rightPathEndX: radius * 0.4 * completion,
-    rightPathEndY: radius * 0.7 + radius * -0.3 * completion
-  };
-}
-
-export default React.createClass({
-  displayName: 'BackButton',
-
-  propTypes: {
-    size: PropTypes.number.isRequired,
-    thickness: PropTypes.number.isRequired,
-    color: PropTypes.string.isRequired,
-    transitionDuration: PropTypes.number.isRequired,
-    outline: PropTypes.bool.isRequired
-  },
-
-  mixins: [tweenState.Mixin],
-
-  getDefaultProps() {
-    return {size: 52, thickness: 2, color: '#000', transitionDuration: 1600, outline: true};
-  },
-
-  getInitialState() {
-    return {completion: 0};
-  },
-
-  onMouseEnter() {
-    let {transitionDuration} = this.props;
-
-    this.tweenState('completion', {
-      easing: easingTypes.easeOutElastic,
-      stackBehavior: stackBehavior.ADDITIVE,
-      duration: transitionDuration,
-      endValue: 1
-    });
-  },
-
-  onMouseLeave() {
-    let {transitionDuration} = this.props;
-
-    this.tweenState('completion', {
-      easing: easingTypes.easeInQuad,
-      stackBehavior: stackBehavior.ADDITIVE,
-      duration: transitionDuration / 8,
-      endValue: 0
-    });
-  },
-
-  render() {
-    let {size, thickness, color, outline} = this.props;
-
-    let originX = size / 2;
-    let originY = size / 2;
-    let radius = size / 2 - thickness;
+    let originX       = size / 2;
+    let originY       = size / 2;
+    let radius        = size / 2 - thickness;
     let circumference = 2 * radius * Math.PI;
 
-    let values = computeValues(this.getTweeningValue('completion'), this.props);
+    return {
+        circleDashOffset:   circumference * completion,
+        circleRotation:     -90 + 180 * completion,
+        firstBarY:          originY - radius * 0.3 * (1 - completion),
+        firstBarRotation:   45 * completion,
+        secondBarMidLength: radius * 0.5 - completion * radius * 0.5,
+        thirdBarY:          originY + radius * 0.3 * (1 - completion),
+        thirdBarRotation:   -45 * completion,
+        barMidLength:       radius * 0.5 + completion * radius * 0.2
+    };
+}
 
-    let {
-      circleDashOffset,
-      circleRotation,
-      leftPathRotation,
-      rightPathRotation,
-      rightPathX,
-      rightPathStartX,
-      rightPathStartY,
-      rightPathEndX,
-      rightPathEndY
-    } = values;
 
-    let leftPathLine = pathCommands([
-      {
-        command: 'M',
-        x: radius * -0.7,
-        y: 0
-      }, {
-        command: 'L',
-        x: radius * 0.7,
-        y: 0
-      }
-    ]);
+export default React.createClass({
+    displayName: 'NavButton',
 
-    let rightPathLine = pathCommands([
-      {
-        command: 'M',
-        x: rightPathStartX,
-        y: rightPathStartY
-      }, {
-        command: 'L',
-        x: 0,
-        y: 0
-      }, {
-        command: 'L',
-        x: rightPathEndX,
-        y: rightPathEndY
-      }
-    ]);
+    propTypes: {
+        size:               PropTypes.number.isRequired,
+        thickness:          PropTypes.number.isRequired,
+        color:              PropTypes.string.isRequired,
+        transitionDuration: PropTypes.number.isRequired,
+        outline:            PropTypes.bool.isRequired
+    },
 
-    let circle = null;
-    if (outline === true) {
-      circle = (
-        <g transform={`translate(${originX}, ${originY}) rotate(${circleRotation}, 0, 0)`}>
-          <circle fill="none" stroke={color} strokeWidth={thickness} r={radius} style={{
-            strokeDasharray: circumference,
-            strokeDashoffset: `${circleDashOffset}`
-          }}/>
-        </g>
-      );
+    mixins: [tweenState.Mixin],
+
+    getDefaultProps() {
+        return {
+            size:               52,
+            thickness:          2,
+            color:              '#000',
+            transitionDuration: 1600,
+            outline:            true
+        };
+    },
+
+    getInitialState() {
+        return {
+            completion: 0
+        };
+    },
+
+    onClick() {
+        let { transitionDuration } = this.props;
+        let { completion } = this.state;
+
+        this.tweenState('completion', {
+            easing:        easingTypes.easeOutElastic,
+            stackBehavior: stackBehavior.ADDITIVE,
+            duration:      transitionDuration,
+            endValue:      completion === 1 ? 0 : 1
+        });
+    },
+
+    render() {
+        let { size, thickness, color, outline } = this.props;
+
+        let originX       = size / 2;
+        let originY       = size / 2;
+        let radius        = size / 2 - thickness;
+        let circumference = 2 * radius * Math.PI;
+
+        let completion = this.getTweeningValue('completion');
+        let values     = computeValues(completion, this.props);
+
+        let {
+            circleDashOffset, circleRotation,
+            firstBarY, thirdBarY,
+            barMidLength, secondBarMidLength,
+            firstBarRotation, thirdBarRotation
+        } = values;
+
+        let firstBarLine = pathCommands([
+            { command: 'M', x: -barMidLength, y: 0 },
+            { command: 'L', x:  barMidLength, y: 0 }
+        ]);
+
+        let secondBarLine = pathCommands([
+            { command: 'M', x: -secondBarMidLength, y: 0 },
+            { command: 'L', x:  secondBarMidLength, y: 0 }
+        ]);
+
+        let thirdBarLine = pathCommands([
+            { command: 'M', x: -barMidLength, y: 0 },
+            { command: 'L', x:  barMidLength, y: 0 }
+        ]);
+
+        let circle = null;
+        if (outline === true) {
+            circle = (
+                <g transform={`translate(${ originX }, ${ originY }) rotate(${ circleRotation }, 0, 0)`}>
+                    <circle
+                        fill="none" stroke={color} strokeWidth={thickness} r={radius}
+                        style={{ strokeDasharray: circumference, strokeDashoffset: `${ circleDashOffset }`}}
+                    />
+                </g>
+            );
+        }
+
+        return (
+            <span className="plus_button">
+                <svg
+                    width={size} height={size} xmlns="http://www.w3.org/svg/2000"
+                    onClick={this.onClick}
+                >
+                    {circle}
+                    <g transform={`translate(${ originX }, ${ firstBarY }) rotate(${ firstBarRotation }, 0, 0)`}>
+                        <path fill="none" stroke={color} strokeWidth={thickness} d={firstBarLine} />
+                    </g>
+                    <g transform={`translate(${ originX }, ${ originY }) rotate(0, 0, 0)`}>
+                        <path fill="none" stroke={color} strokeWidth={thickness} d={secondBarLine} />
+                    </g>
+                    <g transform={`translate(${ originX }, ${ thirdBarY }) rotate(${ thirdBarRotation }, 0, 0)`}>
+                        <path fill="none" stroke={color} strokeWidth={thickness} d={thirdBarLine} />
+                    </g>
+                </svg>
+            </span>
+        );
     }
-
-    return (
-      <span className="back_button">
-        <svg width={size} height={size} xmlns="http://www.w3.org/svg/2000" onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
-          {circle}
-          <g transform={`translate(${originX}, ${originY}) rotate(${leftPathRotation}, 0, 0)`}>
-            <path fill="none" stroke={color} strokeWidth={thickness} d={leftPathLine}/>
-          </g>
-          <g transform={`translate(${rightPathX}, ${originY}) rotate(${rightPathRotation}, 0, 0)`}>
-            <path fill="none" stroke={color} strokeWidth={thickness} d={rightPathLine}/>
-          </g>
-        </svg>
-      </span>
-    );
-  }
 });
