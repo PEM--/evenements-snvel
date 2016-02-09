ca47ea13d6169203e5a13c816322d83afb5ad73a
PEM-- <pemarchandet@gmail.com>
2016-02-09 10:54:05 +0100

    MainMenu - pending

--- app/modules/welcome/MainMenu.jsx
+++ app/modules/welcome/MainMenu.jsx
@@ -1,10 +1,23 @@
+const MenuItem = ({icon, text, link}) => (
+  <a href={link} className='MenuItem'>
+    <i className={`fa fa-${icon}`}></i>
+    <span>{text}</span>
+  </a>
+);
+
 const MainMenu = ({isMenuOpen, onMenuToggle}) => {
   return (
     <nav className={classNames('MainMenu', {isOpen: isMenuOpen}, 'lisibility')} >
       <button className='clickable'>
         <i className='fa fa-times fa-2x' onClick={onMenuToggle}></i>
       </button>
-      <p>Menu items</p>
+      {
+        [
+          {icon: 'user', text: 'Connexion', link: '/signon' },
+          {icon: 'calendar', text: 'Evénements', link: '/events' },
+          {icon: 'cog', text: 'Réglages', link: '/admin' }
+        ].map(m => <MenuItem key={m.link} icon={m.icon} text={m.text} link={m.link} />)
+      }
     </nav>
   );
 };
