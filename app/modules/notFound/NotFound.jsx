0fb3f2cee16ffad379c5b1d62ad122c48c74de3b
PEM-- <pemarchandet@gmail.com>
2016-02-12 13:35:45 +0100

    Graphic design - pending

--- app/modules/notFound/NotFound.jsx
+++ app/modules/notFound/NotFound.jsx
@@ -2,7 +2,7 @@ const { MainLayout, AnimatedLink } = MainApp.Views;
 
 const NotFound = () => (
   <div className='NotFound MainContent maximized animated bounceIn flex col textCenter'>
-    <div className='flexItemStaticSize'>
+    <div className='flexItemStaticSize textCenter'>
       <img src='/img/snvel.svg' width='200px' height='96px' alt='Logo SNVEL'/>
     </div>
     <h1 className='flexItemStaticSize lisibility'>Information non-retrouvée</h1>
