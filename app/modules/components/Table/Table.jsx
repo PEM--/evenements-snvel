e90651967a1c0e5ab5723a23ea91e5788af00db5
PEM-- <pemarchandet@gmail.com>
2016-02-12 20:46:11 +0100

    Graphic design - pending

--- app/modules/components/Table/Table.jsx
+++ app/modules/components/Table/Table.jsx
@@ -1,40 +1,38 @@
 let reactKey = 0;
 
+const TableHeader = ({header}) => (
+  <thead>
+    <tr>
+      {header.map(h => <th key={reactKey++}>{h}</th>)}
+    </tr>
+  </thead>
+);
+
+const TableFooter = ({footer}) => (
+  <tfoot>
+    <tr>
+      {footer.map(f => <td key={reactKey++}>{f}</td>)}
+    </tr>
+  </tfoot>
+);
+
+const TableLine = ({col}) => ( <td key={reactKey++}>{col}</td> );
+
 const Table = ({header, items, footer}) => (
   <table>
-    {
-      header ? (
-        <thead>
-          <tr>
-            {header.map(h => <th key={reactKey++}>{h}</th>)}
-          </tr>
-        </thead>
-      ) : ''
-    }
+    { header ? <TableHeader header={header} /> : '' }
     <tbody>
       {
         items.map((line) => (
           <tr key={reactKey++}>
             {
-              line.map(col => (
-                <td key={reactKey++}>
-                  <div>{col}</div>
-                </td>
-              ))
+              line.map(col => <TableLine key={reactKey++} col={col} />)
             }
           </tr>
         ))
       }
     </tbody>
-    {
-      footer ? (
-        <tfoot>
-          <tr>
-            {footer.map(f => <td key={reactKey++}>{f}</td>)}
-          </tr>
-        </tfoot>
-      ) : ''
-    }
+    { footer ? <TableFooter footer={footer} /> : '' }
   </table>
 );
 
