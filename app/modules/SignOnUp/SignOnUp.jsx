2864794b92a4575f6ecbbfc2dcd965d045436d68
PEM-- <pemarchandet@gmail.com>
2016-02-13 23:47:19 +0100

    Signon & Signup - pending

--- app/modules/SignOnUp/SignOnUp.jsx
+++ app/modules/SignOnUp/SignOnUp.jsx
@@ -1,15 +1,18 @@
-const { Input, AnimatedLink } = MainApp.Views;
+const { CheckBox, Input, Button, AnimatedLink } = MainApp.Views;
 
 class SignOnUp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      isSubscriber: false,
+      csoNumber: '',
+      csoNumberError: null,
       email: '',
-      emailError: '',
+      emailError: null,
       password: '',
-      passwordError: '',
+      passwordError: 'Un minimum de 6 caractères',
       confirm: '',
-      confirmError: '',
+      confirmError: null,
       isValidForm: false,
       isSignUp: this.props.isSignUp ? this.props.isSignUp : false
     };
@@ -17,9 +20,13 @@ class SignOnUp extends React.Component {
       'onEmailChange', 'onPasswordChange', 'onConfirmChange'
     ].map(f => this[f] = this[f].bind(this));
   }
-  onEmailChange(e) {}
-  onPasswordChange(e) {}
-  onConfirmChange(e) {}
+  onCsoNumberChange(e) {
+    console.log('e', e);
+    this.setState({csoNumber: e.target.value });
+  }
+  onEmailChange(e) { this.setState({email: e.target.value }); }
+  onPasswordChange(e) { this.setState({password: e.target.value }); }
+  onConfirmChange(e) { this.setState({confirm: e.target.value }); }
   render() {
     const { isSignUp } = this.props;
     return (
@@ -28,8 +35,32 @@ class SignOnUp extends React.Component {
           <h1>{isSignUp ? 'Création de compte' : 'Connexion'}</h1>
           <form>
             <fieldset>
-              <Input/>
+              <div className='fieldsContainer'>
+                <Input
+                  type='email' placeholder='Entrez votre email'
+                  hasError={this.state.errorEmail}
+                  value={this.state.email}
+                  onChange={this.onEmailChange}
+                />
+                <Input
+                  type='password' placeholder='Entrez votre mot de passe'
+                  hasError={this.state.errorPassword}
+                  errorText={this.state.errorPassword}
+                  onChange={this.onPasswordChange}
+                />
+              {
+                isSignUp ? <Input
+                  type='password' placeholder='Confirmer votre mot de passe'
+                  errorText={this.state.errorConfirm}
+                  value={this.state.confirm}
+                  onChange={this.onConfirmChange}
+                /> : ''
+              }
+            </div>
             </fieldset>
+            <Button isDisabled={this.state.isValidForm} >
+              {isSignUp ? 'Je crée mon compte' : 'Je me connecte'}
+            </Button>
           </form>
           {
             !isSignUp ? (
--- app/modules/components/Input/Input.jsx
+++ app/modules/components/Input/Input.jsx
@@ -1,16 +1,22 @@
 const reactKey = 0;
 
-const Input = ({type, label, placeholder, hasError, errorText, value, onChange}) => (
-  <div className={classNames('formGroup', {'error': hasError})}>
-    {
-      label ? <label htmlFor={`reactKey_${reactKey}`}>{label}</label> : ''
-    }
-    <input
-      id={`reactKey_${reactKey}`} type={type} placeholder={placeholder}
-      value={value} onChange={onChange}
-    />
-    <span className='errorText'>{errorText}</span>
-  </div>
-);
+const Input = ({type, label = null, placeholder, hasError = false, errorText = null, value, onChange}) => {
+  const inLabel = label ? label : `${placeholder} :`;
+  const inError = hasError ? hasError : errorText !== null;
+  return (
+    <div className={classNames('formGroup', {'error': inError})}>
+      {
+        inLabel ? <label htmlFor={`reactKey_${reactKey}`}>{inLabel}</label> : ''
+      }
+      <input
+        id={`reactKey_${reactKey}`} type={type} placeholder={placeholder}
+        value={value} onChange={onChange}
+      />
+      {
+        errorText ? <span className='errorText'>{errorText}</span> : ''
+      }
+    </div>
+  );
+};
 
 MainApp.Views.Input = Input;
