9117e83e39999a40ae440c4d91e72d14d385309b
PEM-- <pemarchandet@gmail.com>
2016-02-20 16:39:31 +0100

    Google Drive as BackOffice

---
+++ app/packages/meteor-google-spreadsheets/server/methods.js
@@ -0,0 +1,79 @@
+var path = Npm.require('path');
+var Future = Npm.require(path.join('fibers', 'future'));
+var fs = Npm.require('fs');
+var pemFile;
+if (__meteor_bootstrap__ && __meteor_bootstrap__.serverDir) {
+  pemFile = path.join(__meteor_bootstrap__.serverDir, 'assets/app/google-key.pem');
+}
+
+Meteor.methods({
+  // Fetches from a google spreadsheet
+  // key = key from googe spreadsheet publish
+  // worksheet = 0-based index of worksheet
+  // range = "R1C1:R5C5"
+  // headerRow = 1-based index of row containing header, if there is a header
+  'spreadsheet/fetch2': function (spreadsheetName, worksheetId, options) {
+    check(spreadsheetName, String);
+    check(worksheetId, Match.OneOf(String, Number));
+    check(options, Object);
+    var fut = new Future(); //don't return until we're done exporting
+
+    EditGoogleSpreadsheet.load({
+      //debug: true,
+      spreadsheetName: spreadsheetName,
+      worksheetId: worksheetId,
+      oauth : {
+        email: options.email,
+        keyFile: pemFile
+      }
+    }, function sheetReady(err, spreadsheet) {
+      if (err) {
+        console.log(err);
+        fut.return(false);
+        return;
+      }
+      spreadsheet.receive(function(err, rows, info) {
+        if (err) {
+          console.log(err);
+          fut.return(false);
+        } else {
+          fut.return({rows: rows, info: info});
+        }
+      });
+    });
+    return fut.wait();
+  },
+  'spreadsheet/update': function (spreadsheetName, worksheetId, updateObject, options) {
+    check(spreadsheetName, String);
+    check(worksheetId, Match.OneOf(String, Number));
+    check(updateObject, Object);
+    check(options, Object);
+    var fut = new Future(); //don't return until we're done exporting
+
+    EditGoogleSpreadsheet.load({
+      //debug: true,
+      spreadsheetName: spreadsheetName,
+      worksheetId: worksheetId,
+      oauth : {
+        email: options.email,
+        keyFile: pemFile
+      }
+    }, function sheetReady(err, spreadsheet) {
+      if (err) {
+        console.log(err);
+        fut.return(false);
+        return;
+      }
+      spreadsheet.add(updateObject);
+      spreadsheet.send(function(err) {
+        if (err) {
+          console.log(err);
+          fut.return(false);
+        } else {
+          fut.return(true);
+        }
+      });
+    });
+    return fut.wait();
+  }
+});
