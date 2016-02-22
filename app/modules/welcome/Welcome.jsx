d84a476472518565657a081df9f4676e59ee5ea4
PEM-- <pemarchandet@gmail.com>
2016-02-22 05:21:48 +0100

    Welcome simplified

--- app/modules/welcome/Welcome.jsx
+++ app/modules/welcome/Welcome.jsx
@@ -1,36 +1,6 @@
 const { Views, Col } = MainApp;
 const { Spinner, AnimatedLink } = Views;
 
-const animate = () => {
-  console.log('Ready');
-  Meteor.defer(() => {
-    $('.headlines').addClass('animated fadeInUp');
-  });
-};
-
-const DumBWelcome = ({ title, location, period }) => (
-  <section className='maximized MainContent animated fadeInUp'>
-    <div className='headlines lisibility'>
-      <h1 className='headline'>
-        <Textfit
-          style={{width: '100%', maxWidth: '600px', lineHeight: '100%', display: 'inline-block'}}
-          mode={'single'}
-        >
-          {title}
-        </Textfit>
-      </h1>
-      <h2 className='headline lisibility'>
-        <Textfit
-          style={{width: '100%', maxWidth: '600px', lineHeight: '100%', display: 'inline-block'}}
-          mode={'single'} onReady={animate}
-        >
-          {location + ' - ' + period}
-        </Textfit>
-      </h2>
-    </div>
-  </section>
-);
-
 class Welcome extends Views.BaseReactMeteor {
   getMeteorData() {
     if (Meteor.isServer) {
@@ -43,10 +13,38 @@ class Welcome extends Views.BaseReactMeteor {
   }
   constructor(props) {
     super(props);
+    this.animate = this.animate.bind(this);
+  }
+  animate() {
+    console.log('Ready');
+    Meteor.defer(() => {
+      $('.headlines').addClass('animated fadeInUp');
+    });
   }
   render() {
     const { title, location, period } = this.data.program;
-    return <DumBWelcome title={title} location={location} period={period} />;
+    return (
+      <section className='maximized MainContent animated fadeInUp'>
+        <div className='headlines lisibility'>
+          <h1 className='headline'>
+            <Textfit
+              style={{width: '100%', maxWidth: '600px', lineHeight: '100%', display: 'inline-block'}}
+              mode={'single'}
+            >
+              {title}
+            </Textfit>
+          </h1>
+          <h2 className='headline lisibility'>
+            <Textfit
+              style={{width: '100%', maxWidth: '600px', lineHeight: '100%', display: 'inline-block'}}
+              mode={'single'} onReady={this.animate}
+            >
+              {location + ' - ' + period}
+            </Textfit>
+          </h2>
+        </div>
+      </section>
+    );
   }
 }
 
