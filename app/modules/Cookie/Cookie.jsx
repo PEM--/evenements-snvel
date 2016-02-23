21ec5cb4cbb313a71a31b5befe004fa527644c7e
PEM-- <pemarchandet@gmail.com>
2016-02-23 14:47:06 +0100

    Cookie - pending

--- app/modules/Cookie/Cookie.jsx
+++ app/modules/Cookie/Cookie.jsx
@@ -1,20 +1,25 @@
 const { Views } = MainApp;
+const { Button, AnimatedLink } = Views;
 
 class Cookie extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return (<div className='Cookie'>
-      <div className='CookieDisclaimer maximized'>
-        <p>
-          <i className='fa fa-bullhorn' />
-          <span className='content'>
-            {Meteor.settings.public.cookie.text}
-          </span>
-        </p>
+    return (
+      <div className='Cookie'>
+        <div className='CookieDisclaimer'>
+          <i className='fa fa-bullhorn flexItemStaticSize' />
+          <div className='content flexItemDynamicSize'>
+            <p className='lisibility'>
+              {Meteor.settings.public.cookie.text}
+            </p>
+            <Button className='btn'>J'accepte</Button>
+            <AnimatedLink to='/cookie' isCream={true}>En savoir plus</AnimatedLink>
+          </div>
+        </div>
       </div>
-    </div>);
+    );
   }
 }
 
