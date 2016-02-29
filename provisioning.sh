d1d3a77cef205a6ecae356897020a02f0d8625fd
PEM-- <pemarchandet@gmail.com>
2016-02-29 10:26:29 +0100

    Docker - pending

--- provisioning.sh
+++ provisioning.sh
@@ -3,7 +3,7 @@
 # Update system
 apt-get update
 apt-get upgrade -y
-apt-get install mailutils -y
+apt-get install mailutils ntp -y
 # Remove and clean unused packages
 apt-get remove exim4 -y
 apt-get autoremove -y
@@ -19,12 +19,12 @@ mkdir /var/postfix
 # Firewall
 apt-get install ufw -y
 ufw allow ssh       # SSH
-ufw allow 80/tcp    # HTTP
-ufw allow 443/tcp   # HTTPS
+ufw allow 80/tcp    # HTTP & WS
+ufw allow 443/tcp   # HTTPS & WSS
 ufw allow 2376/tcp  # Docker
 ufw allow 53        # DNS
-ufw allow 25        # SMTP
-ufw allow 587       # SMTP over TLS
+# ufw allow 25        # SMTP
+ufw allow 587       # ESMTP (SMTP over TLS)
 ufw allow 143       # IMAP2/4
 ufw allow 993       # IMAP2/4 over TLS
 sed -i -e "s/^DEFAULT_FORWARD_POLICY=\"DROP\"/DEFAULT_FORWARD_POLICY=\"ACCEPT\"/" /etc/default/ufw
