c857e3c4594cb62eb4878a77774c887477413076
PEM-- <pemarchandet@gmail.com>
2016-03-07 07:31:20 +0100

    Alerts

---
+++ app/modules/components/Alert/sAlert.js
@@ -0,0 +1,12 @@
+const OPTIONS = {
+  position: 'bottom-right',
+  effect: 'bouncyflip',
+  timeout: 3000
+};
+
+sAlert = {
+  info(text) { MainApp.Views.Alert.info(text, OPTIONS); },
+  success(text) { MainApp.Views.Alert.success(text, OPTIONS); },
+  warning(text) { MainApp.Views.Alert.warning(text, OPTIONS); },
+  error(text) { MainApp.Views.Alert.error(text, OPTIONS); }
+};
