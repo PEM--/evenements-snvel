b8f395cdd7eeda63c268a00518d794937accabe8
PEM-- <pemarchandet@gmail.com>
2016-03-07 07:49:43 +0100

    Admin - Prevent actions while updating

--- app/modules/Admin/Admin.jsx
+++ app/modules/Admin/Admin.jsx
@@ -4,6 +4,8 @@ const { AnimatedLink, Spinner, Table, Button } = Views;
 class Admin extends Views.BaseReactMeteor {
   constructor(props) {
     super(props);
+    this.state = { disabled: false };
+    this.errorSuccess = this.errorSuccess.bind(this);
   }
   getMeteorData() {
     if (Meteor.isServer) {
@@ -11,30 +13,56 @@ class Admin extends Views.BaseReactMeteor {
     }
     return { user: Meteor.user() };
   }
+  errorSuccess(err) {
+    this.setState({disabled: false});
+    if (err) {
+      return sAlert.error('Erreur lors de la mise à jour');
+    }
+    sAlert.success('Mise à jour réussie');
+  }
   onUpdateMarkdown(slug) {
-    return () => Meteor.call('basicPages.update', slug);
+    return () => {
+      this.setState({disabled: true});
+      Meteor.call('basicPages.update', slug, this.errorSuccess);
+    };
   }
   onUpdateMethod(methodName) {
-    return () => Meteor.call(methodName);
+    return () => {
+      this.setState({disabled: true});
+      Meteor.call(methodName, this.errorSuccess);
+    };
   }
   render() {
     const items = Utils.BASIC_PAGES.map(p => (
       [
         p.title,
-        <Button iconName='refresh' className='btn' onClick={this.onUpdateMarkdown(p.slug)}>Mettre à jour</Button>
+        <Button
+          iconName='refresh' className='btn'
+          onClick={this.onUpdateMarkdown(p.slug)}
+          isDisabled={this.state.disabled}
+        >
+          Mettre à jour
+        </Button>
       ]
     )).concat([
       { title: 'Programmes', method: 'programs.update' },
       { title: 'Typologie participants', method: 'userTypes.update' }
     ].map(m => [
-      m.title, <Button iconName='refresh' className='btn' onClick={this.onUpdateMethod(m.method)}>Mettre à jour</Button>
+      m.title,
+      <Button
+        iconName='refresh' className='btn'
+        onClick={this.onUpdateMethod(m.method)}
+        isDisabled={this.state.disabled}
+      >
+        Mettre à jour
+      </Button>
     ]));
     return (
       <section className='maximized MainContent animated fadeIn'>
         {
           this.data.user ? (
             <div className='animated fadeIn lisibility'>
-              <h1>Admin</h1>
+              <h1>Administration</h1>
               <Table
                 header={['Description', 'Action']}
                 items={items}
