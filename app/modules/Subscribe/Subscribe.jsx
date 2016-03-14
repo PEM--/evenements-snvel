7af1dce719882f817176651a196f4e0ae2ee1bdd
PEM-- <pemarchandet@gmail.com>
2016-03-14 08:33:42 +0100

    Display events in Program

--- app/modules/Subscribe/Subscribe.jsx
+++ app/modules/Subscribe/Subscribe.jsx
@@ -15,8 +15,14 @@ class Subscribe extends BaseReactMeteor {
     let line = 0;
     const { user, program } = this.data;
     const userType = user.profile.category;
+    // const price = user ? program.priceRights.find(p => p.code === c.code) : null;
+    // const priceAmount = user ? price.byType.find(t => t.category === userType).amount : 0;
+    // const discount = program.discounts.find(d => d.code === c.code);
+    // const discountAmount = discount.byType.find(t => t.category === userType).amount;
+
     return program.priceRights.reduce((acc, p) => {
       const priceAmount = p.byType.find(t => t.category === userType).amount;
+      // const program
       if (priceAmount !== -1) {
         acc.push([
           <article className='title' key={line++}>
@@ -26,51 +32,6 @@ class Subscribe extends BaseReactMeteor {
           <CheckBox>Je m'inscrits</CheckBox>
         ]);
       }
-      return acc;
-    }, []);
-  }
-  getEvents() {
-    let line = 0;
-    const { user, program } = this.data;
-    const userType = user.profile.category;
-    return program.events.reduce((acc, e) => {
-      e.sessions.forEach(s => {
-        s.conferences.forEach(c => {
-          const price = program.priceRights.find(p => p.code === c.code);
-          const discount = program.discounts.find(d => d.code === c.code);
-          const priceAmount = price.byType.find(t => t.category === userType).amount;
-          const discountAmount = discount.byType.find(t => t.category === userType).amount;
-          if (priceAmount !== -1) {
-            acc.push([
-              <article className='title' key={line++}>
-                <h2 className='event'>{e.title}<span className='session'>{s.title}</span></h2>
-                <p className='conference'>{c.title}</p>
-                <p className='description'>{c.description}</p>
-                {
-                  c.moderator ? (<div className='moderator'>
-                    <i className='fa fa-user-md'></i>
-                    <span>{c.moderator}</span>
-                  </div>) : ''
-                }
-                {
-                  c.speaker ? (<p className='speakers'>
-                    <i className='fa fa-users'></i>
-                  <span>{c.speaker}</span>
-                  </p>) : ''
-                }
-                <p className='hours'>
-                  <i className='fa fa-clock-o'></i>
-                  <span className='date'>{moment(c.begin).format('DD/MM/YY')}</span>
-                  <span className='begin'>{moment(c.begin).format('HH:mm')}</span>
-                  <span className='end'>{moment(c.end).format('HH:mm')}</span>
-                </p>
-              </article>,
-              <div className='price'>{numeralAmountFormat(priceAmount)}</div>,
-              <CheckBox>Je m'inscrits</CheckBox>
-            ]);
-          }
-        });
-      });
       return acc;
     }, []);
   }
--- app/modules/components/Events/Events.jsx
+++ app/modules/components/Events/Events.jsx
@@ -1,47 +1,40 @@
-const Events = ({user = null, program}) => {
+const Events = ({events}) => {
   let line = 0;
-  const userType = user ? user.profile.category : '';
-  const events = program.events.reduce((acc, e) => {
+  const eventTags = events.reduce((acc, e) => {
     e.sessions.forEach(s => {
       s.conferences.forEach(c => {
-        const price = user ? program.priceRights.find(p => p.code === c.code) : null;
-        const priceAmount = user ? price.byType.find(t => t.category === userType).amount : 0;
-        // const discount = program.discounts.find(d => d.code === c.code);
-        // const discountAmount = discount.byType.find(t => t.category === userType).amount;
-        if (priceAmount !== -1) {
-          acc.push(
-            <div className='Event' key={line++}>
-              <h2 className='event'>{e.title}<span className='session'>{s.title}</span></h2>
-              <p className='conference'>{c.title}</p>
-              <p className='description'>{c.description}</p>
-              {
-                c.moderator ? (<div className='moderator'>
-                  <i className='fa fa-user-md'></i>
-                  <span>{c.moderator}</span>
-                </div>) : ''
-              }
-              {
-                c.speaker ? (<p className='speakers'>
-                  <i className='fa fa-users'></i>
-                <span>{c.speaker}</span>
-                </p>) : ''
-              }
-              <p className='hours'>
-                <i className='fa fa-clock-o'></i>
-                <span className='date'>{moment(c.begin).format('DD/MM/YY')}</span>
-                <span className='begin'>{moment(c.begin).format('HH:mm')}</span>
-                <span className='end'>{moment(c.end).format('HH:mm')}</span>
-              </p>
-            </div>
-          );
-        }
+        acc.push(
+          <div className='Event' key={line++}>
+            <h2 className='event'>{e.title}<span className='session'>{s.title}</span></h2>
+            <p className='conference'>{c.title}</p>
+            <p className='description'>{c.description}</p>
+            {
+              c.moderator ? (<div className='moderator'>
+                <i className='fa fa-user-md'></i>
+                <span>{c.moderator}</span>
+              </div>) : ''
+            }
+            {
+              c.speaker ? (<p className='speakers'>
+                <i className='fa fa-users'></i>
+              <span>{c.speaker}</span>
+              </p>) : ''
+            }
+            <p className='hours'>
+              <i className='fa fa-clock-o'></i>
+              <span className='date'>{moment(c.begin).format('DD/MM/YY')}</span>
+              <span className='begin'>{moment(c.begin).format('HH:mm')}</span>
+              <span className='end'>{moment(c.end).format('HH:mm')}</span>
+            </p>
+          </div>
+        );
       });
     });
     return acc;
   }, []);
   return (
     <div className='Events'>
-      {events.map(e => e)}
+      {eventTags.map(e => e)}
     </div>
   );
 };
