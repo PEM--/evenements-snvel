e06c1c16ed847e353f576c7a4d737477323b61e8
PEM-- <pemarchandet@gmail.com>
2016-03-16 13:36:26 +0100

    Payment - pending

--- app/modules/Payment/Payment.jsx
+++ app/modules/Payment/Payment.jsx
@@ -4,6 +4,13 @@ const { AnimatedLink, Select, Input, Button, BaseReactMeteor } = Views;
 class Payment extends BaseReactMeteor {
   constructor(props) {
     super(props);
+    this.state = {
+      paymentType: 'card'
+    };
+    this.onPaymentChange = this.onPaymentChange.bind(this);
+  }
+  onPaymentChange(e) {
+    this.setState({paymentType: e});
   }
   getMeteorData() {
     if (Meteor.isServer) {
@@ -21,6 +28,27 @@ class Payment extends BaseReactMeteor {
     return (
       <section className='maximized MainContent Payment animated fadeIn'>
         <h1>Paiement</h1>
+        <Select
+          value={this.state.paymentType}
+          onChange={this.onPaymentChange}
+          placeholder='Sélectionnez un type de paiment'
+          options={[
+            { value: 'check', label: 'Paiement par chèque' },
+            { value: 'card', label: 'Paiment par carte' }
+          ]}
+        />
+        {
+          this.state.paymentType === 'check' ?
+            <div className='check animated fadeIn'>
+              <h2>Paiement par chèque sélectionné</h2>
+            </div> : ''
+        }
+        {
+          this.state.paymentType === 'card' ?
+            <div className='card animated fadeIn'>
+              <h2>Paiement par carte sélectionné</h2>
+            </div> : ''
+        }
         <AnimatedLink to='subscribe'>Retour aux inscription</AnimatedLink>
       </section>
     );
