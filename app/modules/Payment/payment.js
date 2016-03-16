ec1d0a9779947f73873328fcec699773507aebac
PEM-- <pemarchandet@gmail.com>
2016-03-16 23:00:36 +0100

    Payment - pending

--- app/modules/Payment/payment.js
+++ app/modules/Payment/payment.js
@@ -1,15 +1,15 @@
 initPayment = () => {
   const CardSchema = new SimpleSchema({
     number: {
-      type: String, label: 'N° de carte', min: 15, max: 16, defaultValue: '',
+      type: String, label: 'N° de carte', min: 15, max: 20, defaultValue: '',
       view: { name: 'Input', type: 'text', placeholder: 'N° de carte' }
     },
     name: {
       type: String, label: 'Nom complet', min: 3, max: 20, defaultValue: '',
       view: { name: 'Input', type: 'text', placeholder: 'Nom complet' }
     },
-    date: {
-      type: String, label: 'Date d\'expiration', min: 5, max: 5, defaultValue: '',
+    expiry: {
+      type: String, label: 'Date d\'expiration', min: 7, max: 7, defaultValue: '',
       view: { name: 'Input', type: 'text', placeholder: 'Date d\'expiration' }
     },
     cvc: {
