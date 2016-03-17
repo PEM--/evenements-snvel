be4a47772cfc58096fa7e06ded5f60e2c88e49b7
PEM-- <pemarchandet@gmail.com>
2016-03-17 23:20:07 +0100

    Invoice - pending

--- app/modules/Invoice/Invoice.jsx
+++ app/modules/Invoice/Invoice.jsx
@@ -11,20 +11,32 @@ class Invoice extends BaseReactMeteor {
       program: Col.Programs.findOne(
         {reference: this.props.program},
         {fields: {
-          title: 1, reference: 1, priceRights: 1, discounts: 1, specialRules: 1, tva: 1
+          title: 1, location: 1, period: 1,
+          reference: 1, priceRights: 1, discounts: 1, specialRules: 1, tva: 1
         }}
       ),
       user: Meteor.user()
     };
   }
   render() {
+    const { title, location, period } = this.data.program;
+    const invoice = Utils.renderInvoice(
+      [
+        {
+          designation: 'Journée étude avec repas 24/03',
+          value: 60
+        }
+      ],
+      [],
+      120,
+      140
+    );
     return (
       <section className='maximized MainContent Invoice animated fadeIn'>
         <h1 className='lisibility'>Résumé de vos droits</h1>
-        <h2 className='lisibility'>{this.data.program.title}</h2>
-        <div className='receipt'>
-          <pre>123</pre>
-        </div>
+        <h2 className='lisibility'>{title}</h2>
+        <h3 className='lisibility'>{location} - {period}</h3>
+        <div className='receipt'><pre>{invoice}</pre></div>
         <AnimatedLink to='/'>Revenez à l'accueil</AnimatedLink>
       </section>
     );
