89ec4c91efa9fcae9e15d30f15246da2dcbdc591
PEM-- <pemarchandet@gmail.com>
2016-03-17 22:03:01 +0100

    Better default admin users

--- app/modules/Users/server/users.js
+++ app/modules/Users/server/users.js
@@ -3,12 +3,7 @@ if (Meteor.users.find().count() === 0) {
   console.log('Creating default users');
   Meteor.settings.private.admins.forEach(admin => {
     const adminId = Accounts.createUser({
-      profile: {
-        category: admin.profile.category,
-        firstName: admin.profile.firstName,
-        name: admin.profile.lastName,
-        programs: []
-      },
+      profile: Object.assign({programs: []}, admin.profile),
       username: admin.username,
       password: admin.password,
       email: admin.email
