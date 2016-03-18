fbc2061ba9d9c8143a0e0b0c2c9e9468f3b92430
PEM-- <pemarchandet@gmail.com>
2016-03-18 11:10:03 +0100

    Styled CGV in emails

--- app/modules/Email/server/Email.js
+++ app/modules/Email/server/Email.js
@@ -30,14 +30,14 @@ const billingHtml = s(globalReplacement(Assets.getText('emails/billing.html')))
 
 // Configure accounts
 Accounts.emailTemplates.from = sender;
-Accounts.emailTemplates.verifyEmail.subject = () => confirmationTitle;
+Accounts.emailTemplates.verifyEmail.subject = () => confirmationTitle + '👍';
 Accounts.emailTemplates.verifyEmail.html = (user, url) => {
   return s.replaceAll(
     confirmationHtml,
     'HTML_TEMPLATE_ACTION_VALIDATE_URL',
     url.replace('/#', ''));
 };
-Accounts.emailTemplates.resetPassword.subject = () => resetPasswordTitle;
+Accounts.emailTemplates.resetPassword.subject = () => resetPasswordTitle + '👍';
 Accounts.emailTemplates.resetPassword.html = (user, url) => {
   return s.replaceAll(
     resetPasswordHtml,
@@ -50,21 +50,30 @@ MainApp.Utils.sendConfirmationEmail = (to, idx) => {
   Email.send({
     from: sender,
     to,
-    subject: confirmationTitle + '🐴',
+    subject: confirmationTitle + '👍',
     html: s.replaceAll(confirmationHtml, 'HTML_TEMPLATE_ACTION_VALIDATE_URL', `${url}confirm/${idx}`)
   });
 };
 
+const addCgvStyles = (cgv) => s(cgv)
+  .replaceAll('<h2 ', '<h2 style="color:#8b8b8b;font-family:Impact,sans-serif;font-size:13px;text-align:left;word-break:normal;" ')
+  .replaceAll('<h3 ', '<h3 style="color:#8b8b8b;font-family:Impact,sans-serif;font-size:12px;text-align:left;word-break:normal;" ')
+  .replaceAll('<h4 ', '<h4 style="color:#8b8b8b;font-family:Impact,sans-serif;font-size:11px;text-align:left;word-break:normal;" ')
+  .replaceAll('<p>', '<p style="color:#8b8b8b;font-family:\'San Francisco\',\'Helvetica Neue\',Roboto,Helvetica,Arial,sans-serif;font-size:11px;font-weight:normal;line-height:19px;margin:0;margin-bottom:10px;padding:0;text-align:justify;">')
+  .replaceAll('<a ', '<a href="/" style="color:#e8e8e8;font-size:11px;text-align:justify;text-decoration:none;" ')
+  .value();
+
 MainApp.Utils.sendBillingEmail = (to, invoice, salesAgreementContent) => {
+  const cgv = addCgvStyles(salesAgreementContent);
   Email.send({
     from: sender,
     to,
-    subject: billingTitle,
+    subject: billingTitle + '👍',
     html: s(billingHtml)
       .replaceAll('HTML_TEMPLATE_BILLING_INVOICE_CONTENT', invoice)
       .replaceAll(
         '<HTML_TEMPLATE_TERMS_OF_SALES_CONTENT></HTML_TEMPLATE_TERMS_OF_SALES_CONTENT>'.toLowerCase(),
-        salesAgreementContent)
+        cgv)
       .value()
   });
 };
