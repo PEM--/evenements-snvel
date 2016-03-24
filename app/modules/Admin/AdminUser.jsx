5d891dcacf4891e6ecd0bb273125f409bb5cd9a2
PEM-- <pemarchandet@gmail.com>
2016-03-24 18:40:46 +0100

    Display user - pending

--- app/modules/Admin/AdminUser.jsx
+++ app/modules/Admin/AdminUser.jsx
@@ -18,29 +18,40 @@ class AdminUser extends Views.BaseReactMeteor {
       <section className='maximized MainContent AdminUser animated fadeIn'>
         {
           user ?
-            <div className='content'>
-              <div className='profile lisibility'>
-                <h1>{user.fullName()}</h1>
-                <p className='email'>                
-                  <a className='AnimatedLink cream' href={`mailto:${user.email()}`}>{user.email()}</a>
-                </p>
-                <p className='address'>
-                  <span>{user.profile.address}</span><br />
+            <div>
+              <div className='content'>
+                <div className='profile lisibility'>
+                  <h1>{user.fullName()}</h1>
+                  <h2 className='textCenter'>{user.profile.organization}</h2>
+                  <p className='email'>                
+                    <a className='AnimatedLink cream' href={`mailto:${user.email()}`}>{user.email()}</a>
+                  </p>
+                  <p className='address'>
+                    <span>{user.profile.address}</span>
+                    <span>{user.profile.addressComplementary}</span>
+                    <span>{`${user.profile.postalCode} ${user.profile.city}`}</span>
+                    <span>{user.profile.country}</span>
+                  </p>
+                  <p className='mobile'>
+                    <a className='AnimatedLink cream' href={`tel:${user.profile.mobile}`}>
+                      {user.profile.mobile}
+                    </a>
+                  </p>
                   {
-                    user.profile.addressComplementary ?
-                    <div>
-                      <span>{user.profile.addressComplementary}</span><br />
-                    </div>: ''
+                    user.profile.phone ?
+                      <p className='phone'>
+                        <a className='AnimatedLink cream' href={`tel:${user.profile.phone}`}>
+                          {user.profile.phone}
+                        </a>
+                      </p> : ''
                   }
-                  <span>{user.profile.postalCode} - {user.profile.city}</span><br />
-                  <span>{user.profile.country}</span><br />
-                </p>
+                </div>
               </div>
+              <hr />
+              <AnimatedLink to='/admin'>Retour</AnimatedLink>
             </div> :
             <Spinner className='maximized' />
         }
-        <hr />
-        <AnimatedLink to='/admin'>Retour</AnimatedLink>
       </section>
     );
   }
--- app/modules/Users/userCollection.js
+++ app/modules/Users/userCollection.js
@@ -266,7 +266,7 @@ initUsers = () => {
   Meteor.users.helpers({
     email() { return this.emails[0].address; },
     isAdmin() { return Roles.userIsInRole(this._id, 'admin'); },
-    fullName() { return this.profile.name + ' ' + this.profile.firstName; }
+    fullName() { return this.profile.firstName + ' ' + this.profile.name; }
   });
 
   Schema.UsersSchema = UsersSchema;
