eeb826bc82b543e12ca756d99ac79dbbe1c1b500
PEM-- <pemarchandet@gmail.com>
2016-03-25 10:37:43 +0100

    Migrate accounts without countries

--- app/modules/migrations/server/migrations.js
+++ app/modules/migrations/server/migrations.js
@@ -2,13 +2,27 @@ Migrations.add({
   version: 1,
   up: function() {
     console.log('Migration: 1...');
-    let usersWithoutUsername = Meteor.users.find({username: {$exists: false}}).fetch();
+    const usersWithoutUsername = Meteor.users.find({username: {$exists: false}}).fetch();
     usersWithoutUsername.forEach(u => {
       const email = u.emails[0].address;
       Meteor.users.update({_id: u._id}, {$set: {username: email}});
       console.log('User', email, 'migrated');
     });
     console.log('Migration: 1 done');
+  }
+});
+
+Migrations.add({
+  version: 2,
+  up: function() {
+    console.log('Migration: 2...');
+    const usersWithoutCountry = Meteor.users.find({'profile.country': {$exists: false}}).fetch();
+    usersWithoutCountry.forEach(u => {
+      const email = u.emails[0].address;
+      Meteor.users.update({_id: u._id}, {$set: {'profile.country': 'France'}});
+      console.log('User', email, 'migrated');
+    });
+    console.log('Migration: 2 done');
   }
 });
 
