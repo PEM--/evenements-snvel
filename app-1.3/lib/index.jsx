55103706faca64e420de965b3a83adbf358f24ac
PEM-- <pemarchandet@gmail.com>
2016-04-12 12:43:11 +0200

    React mounter + Flowrouter

--- app-1.3/lib/index.jsx
+++ app-1.3/lib/index.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import {mount} from 'react-mounter';
 import marked from '/imports/markdown';
 import MainApp from '/imports/namespaces';
 
@@ -15,3 +17,18 @@ if (Meteor.isServer) {
 
 exposed.marked = marked;
 exposed.MainApp = MainApp;
+
+const Layout = ({children}) => (
+  <main>
+    <h1>Main</h1>
+    {children}
+  </main>
+);
+
+
+FlowRouter.route('/', {
+  name: 'home',
+  action() {
+    mount(Layout, {children: (<h2>Children</h2>)});
+  }
+});
