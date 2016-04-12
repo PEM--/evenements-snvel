49bd89e7eb9d8a5aaffd136a1ed8f8f256f0f047
PEM-- <pemarchandet@gmail.com>
2016-04-12 17:41:17 +0200

    Tracker component

--- app-1.3/imports/namespaces/index.js
+++ app-1.3/imports/namespaces/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Tracker from 'tracker-component';
 
 let MainApp = {
@@ -5,7 +6,28 @@ let MainApp = {
   Schema: {},
   Utils: {},
   Views: {
-    BaseReactMeteor: Tracker.Component
+    BaseReactComponent: class BaseReactComponent extends Tracker.Component {
+      constructor(props) {
+        super(props);
+      }
+      cloneWithProps(el) {
+        return React.cloneElement(el, { ...this.state });
+      }
+      render() {
+        const { children } = this.props;
+        const count = React.Children.count(children);
+        if (count === 1) {
+          return this.cloneWithProps(children);
+        }
+        return (
+          <div>
+            {
+              React.Children.map(children, child => this.cloneWithProps(child))
+            }
+          </div>
+        );
+      }
+    }
   }
 };
 
--- app-1.3/lib/index.jsx
+++ app-1.3/lib/index.jsx
@@ -3,9 +3,6 @@ import {mount} from 'react-mounter';
 import marked from '/imports/markdown';
 import MainApp from '/imports/namespaces';
 
-import Tracker from 'tracker-component';
-
-
 console.log('Server and Client');
 
 exposed = null;
@@ -33,58 +30,49 @@ if (Meteor.isServer) {
   });
 }
 
-const Layout = ({children}) => (
-  <main>
-    <h1>Main</h1>
-    {children()}
-  </main>
-);
+if (Meteor.isClient) {
+  const Layout = ({children}) => (
+    <main>
+      <h1>Main</h1>
+      {children()}
+    </main>
+  );
 
-const List = ({name, tests = []}) => (
-  <div>
-    <h3>Content: {name}</h3>
-    <ul>
-      {
-        tests.map((t, i) => <li key={i}>{t.title}</li>)
-      }
-    </ul>
-  </div>
-);
+  const List = ({name, tests = []}) => (
+    <div>
+      <h3>Content: {name}</h3>
+      <ul>
+        {
+          tests.map((t, i) => <li key={i}>{t.title}</li>)
+        }
+      </ul>
+    </div>
+  );
 
-class ListModel extends Tracker.Component {
-  constructor(props) {
-    super(props);
-    this.subscribe('tests');
-    this.autorun(() => {
-      this.setState({tests: Tests.find().fetch()});
-    });
+  class ListModel extends MainApp.Views.BaseReactComponent {
+    constructor(props) {
+      super(props);
+      this.subscribe('tests');
+      this.autorun(() => {
+        this.setState({tests: Tests.find().fetch()});
+      });
+    }
   }
-  render() {
-    const { children } = this.props;
-    const { tests } = this.state;
-    const childrenWithProps = React.Children.map(children, (child) =>
-      React.cloneElement(child, { tests }));
-    return (
-      <div>
-        <h2>ListModel</h2>
-        { childrenWithProps }
-      </div>
-    );
-  }
+
+  FlowRouter.route('/', {
+    name: 'home',
+    action() {
+      mount(Layout, {
+        children() {
+          return (
+            <ListModel>
+              <List name='1st list' />
+              <List name='2nd list' />
+            </ListModel>
+          );
+        }
+      });
+    }
+  });
+
 }
-
-FlowRouter.route('/', {
-  name: 'home',
-  action() {
-    mount(Layout, {
-      children() {
-        return (
-          <ListModel>
-            <List name='1st list' />
-            <List name='2nd list' />
-          </ListModel>
-        );
-      }
-    });
-  }
-});
